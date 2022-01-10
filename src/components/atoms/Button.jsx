c19dfd2c3e12ebd6d52b4c04eb4e8d7b4dc120ed
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-01-10 10:42:37 +0700

    style(src/components/atoms): renaming onclick to onClick (use camelcase)

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -4,20 +4,20 @@ import { memo } from 'react'
 /**
  * a Button component tat will display a button with
  * a blue background color and white text color
- * @param {{children: React.ReactNode onclick: () => void | null, className?: string }} param0
+ * @param {{children: React.ReactNode onClick: () => void | null, className?: string }} param0
  * @returns
  */
 
 const Button = ({
   children,
-  onclick = () => null,
+  onClick = () => null,
   className = 'bg-sky-500 hover:bg-sky-600 text-white ',
   ...props
 }) => {
   return (
     <button
       {...props}
-      onClick={onclick}
+      onClick={onClick}
       className={clsx(
         'inline-flex items-center justify-center space-x-1 md:space-x-1.5',
         'py-2 px-2 sm:px-4 md:py-3 md:px-5 rounded-full',
