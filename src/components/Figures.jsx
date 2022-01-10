97bcd89217afc672201076f4bb0afd5bd283cd7f
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-01-10 11:18:53 +0700

    feat(src/components): add JSDoc

--- src/components/Figures.jsx
+++ src/components/Figures.jsx
@@ -2,11 +2,11 @@ import { memo } from 'react'
 
 /**
  * figure component that will display an image when the data is empty
- * @param {{src: string, onclick: () => any}} param0 src prop of the image that would be rendered, this is required
+ * @param {{src: string, onClick: () => any}} param0 src prop of the image that would be rendered, this is required
  * @returns
  */
-const Figures = ({ src, onclick = () => null, dataCy = '' }) => (
-  <figure onClick={onclick} className='w-1/2 mx-auto cursor-pointer'>
+const Figures = ({ src, onClick = () => null, dataCy = '' }) => (
+  <figure onClick={onClick} className='w-1/2 mx-auto cursor-pointer'>
     <img data-cy={dataCy} src={src} className='w-full object-contain aspect-square' alt='activity' />
   </figure>
 )
