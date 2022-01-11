bb4845b67c724c6abcacd1cd539e5ff1b9a561e3
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-01-11 14:52:11 +0700

    feat: updating dist

--- dist/assets/modalAlertAction.d3c9c93c.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./index.0699fed6.js";const t=e=>({type:o,payload:e});export{t as s};
---
+++ dist/assets/modalAlertAction.ed103513.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index.1a5e5896.js";const s=a=>({type:e,payload:a});export{s};
