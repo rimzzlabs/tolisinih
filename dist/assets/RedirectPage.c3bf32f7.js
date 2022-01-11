ee7b04e3b1111a811d124f04920935c100d688ec
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-01-11 16:01:35 +0700

    feat(src): update dist folder

--- dist/assets/RedirectPage.c3bf32f7.js
+++
@@ -1,1 +0,0 @@
-import{i as t,r,j as a}from"./vendor.b30708ff.js";const o=()=>{const e=t();return r.exports.useEffect(()=>{e("/")},[]),a("h2",{children:"redirecting you to home page..."})};export{o as default};
--- dist/assets/modalAlertAction.9811a7cd.js
+++
@@ -1,1 +0,0 @@
-import{S as s}from"./index.98bf48d7.js";const a=o=>({type:s,payload:o});export{a as s};
