e4dc0112ef14ac6673953c17e4360951c7964596
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2022-01-13 12:01:53 +0700

    feat(dist): update dist folder

--- dist/assets/modalAlertAction.91845cb9.js
+++
@@ -1,1 +0,0 @@
-import{S as o}from"./index.52a13353.js";const t=a=>({type:o,payload:a});export{t as s};
---
+++ dist/assets/modalAlertAction.f76f10af.js
@@ -0,0 +1,1 @@
+import{S as o}from"./index.e81f9d47.js";const t=e=>({type:o,payload:e});export{t as s};
